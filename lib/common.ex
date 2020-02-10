7b6cdd5a200861b74656d685c4d6b269360f39d8
Andi Altendorfer <andreas@altendorfer.at>
2020-02-10 21:51:03 +0100

    WIP with e2020-people and e2020-frontend

--- lib/common.ex
+++ lib/common.ex
@@ -1,5 +1,8 @@
 defmodule Common do
   @moduledoc """
   The common structures and function for communication within an emie-cluster.
+
+
+  `Common.Payload` ... a module to deal with payloads between e2020 services.
   """
 end
