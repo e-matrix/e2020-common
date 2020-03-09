587fa1a47e10c13a6ff36b6436f11dae59fffd36
Andi Altendorfer <andreas@altendorfer.at>
2020-03-09 15:40:06 +0100

    Add ExDoc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Common.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "e2020-Common",
+      source_url: "https://github.com/e-matrix/e2020-common",
+      homepage_url: "https://github.com/e-matrix/e2020",
+      docs: [
+        main: "Common",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,6 +30,7 @@ defmodule Common.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:joken, "~> 2.0"},
       {:jason, "~> 1.0"}
     ]
